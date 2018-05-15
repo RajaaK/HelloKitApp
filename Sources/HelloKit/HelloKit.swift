9cd16d1773bef8483d195cc2de46e63b64a192f5
Bertrand Russell Sakthees <raja.xvalue@gmail.com>
2018-05-15 19:15:07 +0530

    Added multiple function

--- Sources/HelloKit/HelloKit.swift
+++ Sources/HelloKit/HelloKit.swift
@@ -6,7 +6,16 @@ public struct HelloKit {
     public var text = "Hello, World!"
     
     public func addNumbers(a: Int, b: Int) -> Int {
-        
         return a + b
+    }
+    
+    public func subNumbers(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    public func mulNumbers(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    public func divNumbers(a: Int, b: Int) -> Int {
+        return a / b
     }
 }
