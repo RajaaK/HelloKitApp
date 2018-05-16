ba2e718ba676f4122dab005088a4bf8961c919a6
Bertrand Russell Sakthees <raja.xvalue@gmail.com>
2018-05-16 11:47:08 +0530

    Packge

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HelloKit",
-            targets: ["HelloKit"]),
+            name:"Validate",
+            targets: ["HelloKit","Validate"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
